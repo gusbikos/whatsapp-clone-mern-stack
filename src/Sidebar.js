eed56166a0b14a9042eb06d1320845c15b2ce64e
Gus Bikos <gusbikos@gmail.com>
2022-01-18 16:12:18 -0500

    finished sidebar styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarChat from './SidebarChat'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import { Avatar, IconButton } from '@material-ui/core'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { SearchOutlined } from '@material-ui/icons'
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
-            <h1> i am sidebar</h1>
-            <div className='sidebear-header'>
+            <div className='sidebar-header'>
+                <Avatar src='https://avatars.githubusercontent.com/u/70536063?v=4' />
+                
                 <div className='sidebar-headerRight'>
-                    <DonutLargeIcon />
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
+
+            </div>
+
+            <div className='sidebar-search'>
+                <div className='sidebar-searchContainer'>
+                    <SearchOutlined />
+                        <input placeholder='Search or start new chat' type='text' />
+                </div>
+            </div>
+
+            <div className='sidebar-chats'>
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
